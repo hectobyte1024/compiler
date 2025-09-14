17210f3c09ae29080f09ad9b628b3c29cd4cf4f6
Jose Angel Bernal Pucheta <45618676+hectobyte1024@users.noreply.github.com>
2025-09-13 22:10:45 -0600

    Add files via upload

---
+++ sdl_compiler_lib.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/codegen/base_generator.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/codegen/base_generator.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/codegen/base_generator.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/codegen/cuda_generator.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/codegen/cuda_generator.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/codegen/cuda_generator.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/codegen/glsl_generator.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/codegen/glsl_generator.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/codegen/glsl_generator.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/compiler/compiler.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/compiler/compiler.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/compiler/compiler.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/lexer/lexer.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/lexer/lexer.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/lexer/lexer.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/lexer/token.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/lexer/token.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/lexer/token.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/parser/ast.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/parser/ast.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/parser/ast.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/parser/parser.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/parser/parser.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/parser/parser.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/semantic/analyzer.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/semantic/analyzer.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/semantic/analyzer.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/semantic/symbol_table.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/semantic/symbol_table.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/semantic/symbol_table.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/utils/error_handler.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/utils/error_handler.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/utils/error_handler.cpp.o.d"
+  "/home/hectobyte1024/Documents/sdl shader compiler/src/utils/file_utils.cpp" "CMakeFiles/sdl_compiler_lib.dir/src/utils/file_utils.cpp.o" "gcc" "CMakeFiles/sdl_compiler_lib.dir/src/utils/file_utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
